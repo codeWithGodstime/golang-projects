b73b247bbcdd9dea262c8ad4e764aff2feb34e01
codeWithGodstime <specialgodstym@gmail.com>
2025-04-27 19:51:54 +0100

    added other request types

--- mini-postman/core/controller.go
+++ mini-postman/core/controller.go
@@ -10,10 +10,13 @@ func MakeRequestController(resType, url string, headers map[string]string, body 
 	case "GET":
 		return GlobalClient.Get(url, headers)
 	case "POST":
-		return GlobalClient.Post(url, "", headers)
+		return GlobalClient.Post(url, body, headers)
+	case "PUT":
+		return GlobalClient.Request("PUT", url, body, headers)
+	case "PATCH":
+		return GlobalClient.Request("PATCH", url, body, headers)
 	case "DELETE":
-		// return GlobalClient.Request("DELETE", url, "", headers)
-		return nil, nil
+		return GlobalClient.Request("DELETE", url, "", headers)
 	default:
 		return nil, fmt.Errorf("unsupported method: %s", resType)
 	}
